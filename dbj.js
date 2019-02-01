935b33bcd7c9b99c2ae765d3d2519e4752b38a3b
DehuaZhao <edwdzhao@gmail.com>
2019-02-01 12:58:03 +0800

    fix: display with no ratings

--- dbj.js
+++ dbj.js
@@ -3,13 +3,15 @@ var oriRatingElem = document.getElementsByClassName('ll rating_num');
 var oriRating = oriRatingElem[0].outerText;
 
 // converted rating
+if (oriRating != "")
+{
+    var cvtRating = (oriRating * 10 - 2 * 10) * 5 / 4 / 10;
 
-var cvtRating = (oriRating * 10 - 2 * 10) * 5 / 4 / 10;
+    var cvrtElem = document.createElement('div');
+    cvrtElem.appendChild(document.createTextNode(cvtRating));
+    cvrtElem.setAttribute('id', 'dbj_cvtRating_num');
+    cvrtElem.setAttribute('style', 'font-size: 20px;');
 
-var cvrtElem = document.createElement('div');
-cvrtElem.appendChild(document.createTextNode(cvtRating));
-cvrtElem.setAttribute('id', 'dbj_cvtRating_num');
-cvrtElem.setAttribute('style', 'font-size: 20px;');
-
-var ratingRootElem = document.getElementsByClassName('rating_self clearfix');
-ratingRootElem[0].insertBefore(cvrtElem, ratingRootElem[0].childNodes[2]);
+    var ratingRootElem = document.getElementsByClassName('rating_self clearfix');
+    ratingRootElem[0].insertBefore(cvrtElem, ratingRootElem[0].childNodes[2]);
+}
